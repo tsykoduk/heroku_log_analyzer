1157c79355536a517a42bd4d3e3440bb9df8dec4
Greg Nokes <tsykoduk@gmail.com>
2013-01-14 14:38:23 -0800

    laying out the framework

---
+++ runner.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby -wKU
+
+##
+# We want this to run for a certian amount of time
+# pulling the log files from a speific app
+# and then run some analysis on them
+#
+# Find things like # of H12's, # of dyno restarts
+# Longest running transactions, and most importantaly the
+# 99.9, 99 and 50 percentile transaction times
+##
+
+##
+# Pull in command line switches
+#
+
+
+#
+##
+
+##
+# Build out a log run f 
