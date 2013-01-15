ede2c3288d26beb375b1f84eb5c7f737f2543409
Greg Nokes <tsykoduk@gmail.com>
2013-01-15 15:14:47 -0800

    added percentile logic to service times

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -1,12 +1,24 @@
 require 'csv'
 
 file_name = "123"
+percentile_targets[99.9, 99, 80, 50]
+puts
 
 #Get the service times out of the raw logs
 `awk '{for(i=1;i<=NF;i++){if($i~/^service=/){print $i}}}' #{file_name} > #{file_name}_s1`
 `tr '=' ',' <#{file_name}_s1 > #{file_name}_s2`
 `tr 'ms' ',ms'<#{file_name}_s2 > #{file_name}_servicetimes.csv`
 `rm #{file_name}_s1 #{file_name}_s2`
+service_times = []
+service_time_file = CSV.read("#{file_name}_servicetimes.csv")
+service_time_file.each {|row| service_times << row[2]}
+puts "Service Time Percentiles"
+puts
+percentile_targets.each { |p|
+  puts p + percentile_of(service_times, p)
+  }
+
+
 
 #get the wait times out of the logs
 
@@ -32,7 +44,9 @@ heroku_errors.each { |e|
   
 def percentile_of(list_of_numbers[], type=99.9)
   
-  #do magic mathy stuff here
+  list_of_numbers.sort!
+  p = (type/100)
+  return list_of_numbers[p]
   
 end
   
