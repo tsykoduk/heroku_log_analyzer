c8703d099849a09a99b36e0a7049b4489c272204
Greg Nokes <tsykoduk@gmail.com>
2013-02-01 13:26:12 -0800

    add some headers, pull queue depth, update gitigonore

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -29,6 +29,7 @@ end
 def report_generator(file, type,percentile_targets)
   report = "\n"
   report <<  "#{type} times\n"
+  report <<  "\%\tms"
   report <<  "=-=-=-=-=-=-=-=-=-=\n"
   report << generate_list_of_times("#{file}_#{type}times.csv",percentile_targets)
   report << "\n"
@@ -44,8 +45,9 @@ def log_parser(file,percentile_targets,time)
   report << report_generator(file, "service", percentile_targets)
   log_processor(file, "wait")
   report << report_generator(file, "wait", percentile_targets)
-  log_processor(file, "queue")
-  report << report_generator(file, "queue", percentile_targets)
+# Queue is not a time measure, rather a queue depth. It also almost never reports. Pulling it for the time being
+#  log_processor(file, "queue")
+#  report << report_generator(file, "queue", percentile_targets)
   log_processor(file, "connect")
   report << report_generator(file, "connect", percentile_targets)
   
